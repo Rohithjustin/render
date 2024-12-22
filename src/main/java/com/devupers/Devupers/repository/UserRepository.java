899246a3e91d090f12fc5c34db5d728b80f63544
pokala57 <pokalanani2000@gmail.com>
2024-12-22 11:48:02 +0530

    doc

--- src/main/java/com/devupers/Devupers/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devupers.Devupers.repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.devupers.Devupers.model.UserModel;
-
-public interface UserRepository extends MongoRepository<UserModel, String> {
-}
